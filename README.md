f75194d8d0322ba1534ae7a62964f52b6885ce1b
fiddyspence <chris@spence.org.uk>
2015-11-17 15:23:21 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+cyber-spider
+=============
+
+![](logo.png)
+
+Chrome extension that replaces occurrences of 'cyber' with 'spider' - forked from cloud-to-butt
+
+
+[Direct download of crx file](https://github.com/panicsteve/cloud-to-butt/blob/master/CloudToButt.crx?raw=true)
+
+Installation
+------------
+
+In Chrome, choose Window > Extensions.  Drag CloudToButt.crx into the page that appears.
---
+++ Source/content_script.js
@@ -0,0 +1,44 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bcyber(.*)\b/g, "spider$1");
+	v = v.replace(/\bCyber(.*)\b/g, "Spider$1");
+	v = v.replace(/\bcyberspace\b/g, "spiderspace");
+	v = v.replace(/\bCyberspace\b/g, "Spiderspace");
+	v = v.replace(/\bcyber\b/g, "spider");
+	v = v.replace(/\bCyber\b/g, "Spider");
+	
+	textNode.nodeValue = v;
+}
+
+
